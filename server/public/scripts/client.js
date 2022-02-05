e88ff1a772f33ab0c877d955079edba39485f736
Andy Swanson <swan1625@icloud.com>
2022-02-05 10:23:53 -0600

    server set up

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,26 +1,58 @@
 $(document).ready(onReady);
 
 function onReady() {
-    console.log('JQ');
-
+  
+    // getMath();
     $( '#equal-button' ).on( 'click',  runMath);    // equal button click listener
-    // $( '#clear-button' ).on( 'click', clearInputs );
+    // $( '#clear-button' ).on( 'click', clearInputs );   //clear button click listener 
 }
 
 function runMath(){                             // function that 
     console.log('working');
     let numOne = $('#valueOne').val();
     let numTwo = $('#valueTwo').val();
+    let sign = $('#signs').val();
     let mathObj = {                       
     a: numOne,
     b: numTwo,
-    signs: signs
-      }
-
+    signs: sign
+}
+console.log(mathObj);
 if(numOne === '' || numTwo === '') {                                              //alert if input values arent defined
     alert('Please fill out both numbers! / ¡por favor llene ambos números!');
-    return;
+    // return;
 }
+    $.ajax({
+        method: 'POST',
+        url: '/mathProblems',
+        data : mathObj
+    }).then( function(response){
+        console.log('back from post with', response);
+        getMath();
+    }).catch ( function( err ){
+        alert ('error occured, see console for detail');
+        console.log(err);
+    })
+ }    
+
+
+function getMath() {
+    $.ajax({
+        method: 'GET',
+        url: '/mathProblems',
+    }).then( function (response ){
+        console.log('back from get with', response);
+    }).catch ( function( err ){
+        alert ('error occured, see console for detail');
+        console.log(err);
+    })
+ }  //end getMath
+
+//     $.ajax({
+//         method: 'POST',
+//         url: '/guesses',
+//         data: {
+// }
 // if (sign == 'add'){
 //     answer = (a + b);
 // } else if (sign == 'sub' ){
@@ -32,4 +64,11 @@ if(numOne === '' || numTwo === '') {                                            
 // }
 
 
-}
+//    if (signs == 'add'){
+//      signs = '+';
+// } else if (signs == 'sub' ){
+//     signs = '-';
+// } else if (signs == 'mul' ){
+//     signs = '*';
+// }else if (signs == 'div' ){
+//     signs = '*';
--- server/server.js
+++ server/server.js
@@ -5,15 +5,23 @@ const app = express();
 
 const port = 5000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static('server/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+problemArr = [];
 
 
-
-
-
-
-
+app.get ('/mathProblems', function(req, res) { 
+    console.log('in /mathProbs GET');
+    res.send(problemArr);
+ 
+  });
+  
+app.post('/mathProblems', function(req, res) {
+    console.log('in /mathProbs GET', req.body);
+    problemArr.push( req.body );
+    res.sendStatus(200);
+  })
 
 
 // --------------------------------------------
