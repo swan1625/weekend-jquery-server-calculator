4285fd4fef825858e05307d538b55b6f9cb1d167
Andy Swanson <swan1625@icloud.com>
2022-02-04 22:15:05 -0600

    working on equation

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,31 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('JQ');
+
+    $( '#equal-button' ).on( 'click', runMath );    
+    // $( '#clear-button' ).on( 'click', clearInputs );
+}
+
+function runMath(){
+console.log('working');
+
+const InputInformation = {                       
+    a: Number($('#valueOne').val()),
+    b: Number($('#valueTwo').val()),
+    sign: $('#signs').val()
+      }
+let answer;
+
+if (sign == 'add'){
+    answer = (a + b);
+} else if (sign == 'sub' ){
+    answer = (a - b);
+} else if (sign == 'mul' ){
+    answer = (a * b);
+}else if (sign == 'div' ){
+    answer = (a / b);
+}
+
+document.querySelector("#result").value = answer;
+
 }
