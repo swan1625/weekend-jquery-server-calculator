ddcfed3e8581cf3d9d710527a5ee66c743b61ebf
Andy Swanson <swan1625@icloud.com>
2022-02-05 00:07:25 -0600

    need to test server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,30 +3,33 @@ $(document).ready(onReady);
 function onReady() {
     console.log('JQ');
 
-    $( '#equal-button' ).on( 'click', runMath );    
+    $( '#equal-button' ).on( 'click',  runMath);    // equal button click listener
     // $( '#clear-button' ).on( 'click', clearInputs );
 }
 
-function runMath(){
-console.log('working');
-
-const InputInformation = {                       
-    a: Number($('#valueOne').val()),
-    b: Number($('#valueTwo').val()),
-    sign: $('#signs').val()
+function runMath(){                             // function that 
+    console.log('working');
+    let numOne = $('#valueOne').val();
+    let numTwo = $('#valueTwo').val();
+    let mathObj = {                       
+    a: numOne,
+    b: numTwo,
+    signs: signs
       }
-let answer;
 
-if (sign == 'add'){
-    answer = (a + b);
-} else if (sign == 'sub' ){
-    answer = (a - b);
-} else if (sign == 'mul' ){
-    answer = (a * b);
-}else if (sign == 'div' ){
-    answer = (a / b);
+if(numOne === '' || numTwo === '') {                                              //alert if input values arent defined
+    alert('Please fill out both numbers! / ¡por favor llene ambos números!');
+    return;
 }
+// if (sign == 'add'){
+//     answer = (a + b);
+// } else if (sign == 'sub' ){
+//     answer = (a - b);
+// } else if (sign == 'mul' ){
+//     answer = (a * b);
+// }else if (sign == 'div' ){
+//     answer = (a / b);
+// }
 
-document.querySelector("#result").value = answer;
 
 }
